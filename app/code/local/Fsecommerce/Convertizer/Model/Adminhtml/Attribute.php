b52d94dc651d368b554d4e0a0a234661bfedae11
fsecommerce <mschneider@fs-ecommerce.com>
2015-03-12 13:39:13 +0100

    [Convertizer] - add shipping options

---
+++ app/code/local/Fsecommerce/Convertizer/Model/Adminhtml/Attribute.php
@@ -0,0 +1,26 @@
+<?php
+
+class Fsecommerce_Convertizer_Model_Adminhtml_Attribute extends Mage_Adminhtml_Model_System_Store
+{
+    protected $_url;
+
+    public function toOptionArray()
+    {
+        $options = array();
+		
+		
+		$attributes = Mage::getResourceModel('catalog/product_attribute_collection')
+		->getItems();
+
+		foreach ($attributes as $attribute){
+			$attrCode = $attribute->getAttributecode();
+			$options[$attrCode] = $attribute->getFrontendLabel();
+		}
+		
+        
+
+        return $options;
+    }
+
+    
+}
